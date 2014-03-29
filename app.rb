a7ec7f9a2bd77c2d6bfe1dbc3fa9784d8b9f4e35
Joao Sa <joao.sa@innvent.com.br>
2014-03-28 23:48:21 -0300

    Fill rectangles around text box
    - Useful to determine coordinates for future bounding boxes

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'RMagick'
+require 'pry'
 
 module Doge
   class App < Sinatra::Base
@@ -28,8 +29,6 @@ module Doge
     texts.each do |text|
       metrics = caption.get_multiline_type_metrics(text)
 
-      caption.fill = colors.sample
-
       min_x = 0
       max_x = img.columns - metrics.width
       x = Random.rand(min_x..max_x)
@@ -38,11 +37,50 @@ module Doge
       max_y = img.rows - metrics.height + metrics.ascent
       y = Random.rand(min_y..max_y)
 
+      caption.fill = colors.sample
       caption.annotate(img, 
                       metrics.width,
                       metrics.height,
                       x, y,
                       text)
+
+      y = y - metrics.ascent
+
+      square_a = Magick::Draw.new
+      square_a.fill(colors[0])
+      square_a.fill_opacity(0.5)
+      square_a.rectangle(0,
+                         0,
+                         x,
+                         img.rows)
+      square_a.draw(img)
+
+      square_b = Magick::Draw.new
+      square_b.fill(colors[1])
+      square_b.fill_opacity(0.5)
+      square_b.rectangle(0,
+                         0,
+                         img.columns,
+                         y)
+      square_b.draw(img)
+
+      square_c = Magick::Draw.new
+      square_c.fill(colors[2])
+      square_c.fill_opacity(0.5)
+      square_c.rectangle(0,
+                         y + metrics.height,
+                         img.columns,
+                         img.rows)
+      square_c.draw(img)
+      
+      square_d = Magick::Draw.new
+      square_d.fill(colors[3])
+      square_d.fill_opacity(0.5)
+      square_d.rectangle(x + metrics.width,
+                         0,
+                         img.columns,
+                         img.rows)
+      square_d.draw(img)
     end
 
     content_type 'image/jpeg'
